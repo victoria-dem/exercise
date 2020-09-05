589f11212771bf21447ef5d048c0ab5240077387
Victoria Dementyeva <videmy@gmail.com>
2020-09-05 18:56:55 -0400

    fix errors

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function palindrome(passString) {
   const string = passString.toLowerCase().replace(/[^A-Za-zА-Яа-я]/g, "");
-  if (string.length == 0 || typeof string !== "string") {
+  if (string.length === 0 || typeof string !== "string") {
     return undefined;
-  } else {
-    for (let i = 0; i < Math.floor(string.length / 2); i++) {
-      if (string.charAt(i) !== string.charAt(string.length - i - 1)) {
-        return false;
-      }
-      return true;
+  }
+  for (let i = 0; i < Math.floor(string.length / 2); i++) {
+    if (string.charAt(i) !== string.charAt(string.length - i - 1)) {
+      return false;
     }
+    return true;
   }
 }
 
@@ -19,13 +18,15 @@ function palindrome(passString) {
 
 function fizzBuzz(number) {
   for (let i = 1; i <= number; i++) {
-    if (i % 3 == 0 && i % 5 !== 0) {
+    if (i % 3 === 0 && i % 5 !== 0) {
       console.log("fizz");
-    } else if (i % 3 == 0 && i % 5 == 0) {
+    } else if (i % 3 === 0 && i % 5 === 0) {
       console.log("fizzbuzz");
-    } else if (i % 5 == 0) {
+    } else if (i % 5 === 0) {
       console.log("buzz");
-    } else console.log(i);
+    } else {
+      console.log(i);
+    }
   }
 }
 
@@ -35,13 +36,18 @@ function reverseWords(str) {
   const words = str.split(" ");
   let newString = "";
   for (let i = words.length - 1; i >= 0; i--) {
-    i == 0 ? (newString += words[i]) : (newString += words[i] + " ");
+    i === 0 ? (newString += words[i]) : (newString += words[i] + " ");
   }
   return newString;
 }
 
-// console.log(reverseWords('всегда много путей достичь цель есть')); // "есть цель достичь путей много всегда"
-// console.log(reverseWords('испробовать их все должны вы')); // "вы должны все их испробовать"
+// более легкое решение
+function reverseWords1(str) {
+  return str.split(" ").reverse().join(" ");
+}
+
+// console.log(reverseWords1('всегда много путей достичь цель есть')); // "есть цель достичь путей много всегда"
+// console.log(reverseWords1('испробовать их все должны вы')); // "вы должны все их испробовать"
 
 function capitalize(str) {
   function replacer(match) {
@@ -63,9 +69,23 @@ function findVowels(str) {
   });
   return vowelsCount;
 }
-// console.log(findVowels('здравствуй')); // 2
-// console.log(findVowels('привет')); // 2
-// console.log(findVowels('хеллоу')); // 3
+
+// Более правильное решение через set
+function findVowels1(str) {
+  let vowelsCount = 0;
+  const letters = str.split("");
+  const vowelsSet = new Set(["а", "я", "о", "ё", "у", "ю", "ы", "и", "э", "е"]);
+  letters.forEach((letter) => {
+    if (vowelsSet.has(letter)) {
+      vowelsCount++;
+    }
+  });
+  return vowelsCount;
+}
+
+// console.log(findVowels1('хеллоу')); // 3
+// console.log(findVowels1('аааапрт')); // 4
+// console.log(findVowels1('кокококо')); // 4
 
 function uniq(arr) {
   let newArr = [];
@@ -76,26 +96,67 @@ function uniq(arr) {
   });
   return newArr;
 }
+// Лучшее решение через set
 
-// console.log(uniq([1, 2, 3, 7, 7, 4, 5, 5, 1]));
+function uniq1(arr) {
+  const newSet = new Set();
+  arr.forEach((element) => {
+    newSet.add(element);
+  });
+  return Array.from(newSet);
+}
+
+// console.log(uniq1([1, 2, 3, 7, 7, 4, 5, 5, 1]));
 
 function anagram(str1, str2) {
-  const arrayStr1 = str1
-    .toLowerCase()
-    .replace(/[^A-Za-zА-Яа-я]/g, "")
-    .split("");
-  const normalStr2 = str2.toLowerCase().replace(/[^A-Za-zА-Яа-я]/g, "");
+  const Str1 = str1.toLowerCase().replace(/[^A-Za-zА-Яа-я]/g, "");
+
+  const Str2 = str2.toLowerCase().replace(/[^A-Za-zА-Яа-я]/g, "");
+  const obj1 = new Object();
+  const obj2 = new Object();
   for (let i = 0; i <= arrayStr1.length - 1; i++) {
     if (!normalStr2.includes(arrayStr1[i])) {
-      console.log("f");
       return false;
     }
     return true;
   }
 }
 
-// console.log(anagram('finder', 'Friend')); // true
-// console.log(anagram('hello', 'bye')); // false
+// решение чтобы избежать цикла в цикле через сравнивание объектов
+function anagram1(str1, str2) {
+  function toObject(string) {
+    const object = new Object();
+    const array = string.split("");
+    array.forEach((elem) => {
+      if (!object.hasOwnProperty(elem)) {
+        object[elem] = 1;
+      } else {
+        object[elem] += 1;
+      }
+    });
+    return object;
+  }
+
+  function shallowCompare(obj1, obj2) {
+    const key1 = Object.keys(obj1);
+    const key2 = Object.keys(obj2);
+    if (key1.length !== key2.length) {
+      return false;
+    }
+    key1.forEach((elem) => {
+      if (obj1[elem] !== obj2[elem]) {
+        return false;
+      }
+    });
+    return true;
+  }
+  const strReady1 = str1.toLowerCase().replace(/[^A-Za-zА-Яа-я]/g, "");
+  const strReady2 = str2.toLowerCase().replace(/[^A-Za-zА-Яа-я]/g, "");
+  return shallowCompare(toObject(strReady1), toObject(strReady2));
+}
+
+//   console.log(anagram1("finder", "Friend")); // true
+//   console.log(anagram1('hello', 'bye')); // false
 
 function fibonacci(num) {
   let fibonacciPrevios = 0;
@@ -109,23 +170,33 @@ function fibonacci(num) {
   return fibonacciPrevios;
 }
 
-// console.log(fibonacci(7));
+//решение через рекурсию
+function fibonacciRec(num) {
+  if (num === 1) {
+    return 0;
+  }
+  if (num === 2) {
+    return 1;
+  }
+  return fibonacciRec(num - 1) + fibonacciRec(num - 2);
+}
+
+// console.log(fibonacciRec(11))
 
 function isPrime(n) {
   if (n < 2) {
     return false;
-  } 
-  if (n == 2) {
-    return true;
-  } else {
-    for (let i = 2; i < n; i++) {
-      if (n % i == 0) {
-        console.log(n % i);
-        return false;
-      }
-    }
+  }
+  if (n === 2) {
     return true;
   }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      console.log(n % i);
+      return false;
+    }
+  }
+  return true;
 }
 
 // console.log(isPrime(0));
@@ -138,26 +209,31 @@ function isPrime(n) {
 
 function factorial(n) {
   let fact = 1;
-  if (n == 0) {
+  if (n === 0) {
     return fact;
-  } else {
-    for (let i = 1; i <= n; i++) {
-      fact = fact * i;
-    }
+  }
+  for (let i = 1; i <= n; i++) {
+    fact = fact * i;
   }
   return fact;
 }
-
-// console.log(factorial(6))
+// Решение через рекурсию
+function factorialRec(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return (n = n * factorialRec(n - 1));
+}
+//  console.log(factorialRec(6))
 
 function intersection(arr1, arr2) {
-  let newArr = [];
+  const newArr = [];
   arr1.forEach((element) => {
-    for (let i = 0; i <= arr2.length; i++) {
-      if (element === arr2[i]) {
+    arr2.forEach((elem2) => {
+      if (element === elem2) {
         newArr.push(element);
       }
-    }
+    });
   });
   return newArr;
 }
@@ -168,21 +244,22 @@ function intersection(arr1, arr2) {
 function countZeros(n) {
   let count = 0;
   for (let i = 1; i <= n; i++) {
-    let string = i + "";
+    const string = i.toString();
     if (string.includes("0")) {
       count++;
     }
   }
   return count;
 }
+// решение без приведения к строке
 
-// console.log(countZeros(20)); // 2 – два нуля, по одному в числах 10 и 20
-// console.log(countZeros(100)); // 11 – 11 нулей в числах: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+console.log(countZeros(20)); // 2 – два нуля, по одному в числах 10 и 20
+console.log(countZeros(200)); // 11 – 11 нулей в числах: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
 
 function sumOfTwo(arr, num) {
   for (i = 0; i <= arr.length; i++) {
     for (j = 0; j <= arr.length; j++) {
-      if (arr[i] + arr[j] == num) {
+      if (arr[i] + arr[j] === num) {
         return true;
       }
     }
@@ -194,22 +271,21 @@ function sumOfTwo(arr, num) {
 // console.log(sumOfTwo([1, 2, 3, 4, 5], 100)); // false
 
 function primes(n) {
-  let newArr = [];
+  const newArr = [];
   function isPrime(n) {
     if (n < 2) {
       return false;
-    } 
-    if (n == 2) {
-      return true;
-    } else {
-      for (let i = 2; i < n; i++) {
-        if (n % i == 0) {
-          console.log(n % i);
-          return false;
-        }
-      }
+    }
+    if (n === 2) {
       return true;
     }
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) {
+        console.log(n % i);
+        return false;
+      }
+    }
+    return true;
   }
   for (let i = 1; i <= n; i++) {
     if (isPrime(i)) {
@@ -217,6 +293,7 @@ function primes(n) {
     }
   }
   return newArr;
+  return n.filter(isPrime);
 }
 // console.log(primes(6)); // [2, 3, 5]
 // console.log(primes(17)); // [2, 3, 5, 7, 11, 13, 17]
